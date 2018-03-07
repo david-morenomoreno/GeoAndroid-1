ab69a60edb4277d6423b05518d761d65e524cc7f
Unknown <da17mo@gmail.com>
2018-03-07 13:07:12 +0100

    Documentation of function

--- GeoForensic.py
+++ GeoForensic.py
@@ -1,80 +1,84 @@
-import argparse, requests
-from bs4 import BeautifulSoup
+import argparse
+import os
+import requests
 import sqlite3
-import os, re
-import numpy as np
 
 
-#-----Conection Sqlite database
-def selectQuery(dbPath, query):
-    conn = sqlite3.connect(dbPath)
-    conn.row_factory = lambda cursor, row: row[0]
+#Constant
+
+YOUR_API_KEY = "AIzaSyBgqJdq2ZB2wxaYyxrJBnZeFC0ytINjaUE"
+URL = "https://www.googleapis.com/geolocation/v1/geolocate?key="
+
+WIGLE_WIFI_QUERY = "select lastlat, lastlon, bssid from network group by bssid"
+HERREAVAD_WIFI_QUERY = "select bssid from local_reports group by bssid"
+HERREAVAD_CELL_QUERY  = "select rowkey from lru_table group by rowkey"
+
+
+
+def SQLite(Path, Query):
+    '''
+    This fuction return de result of the query of SQLite
+        :param Path: The path of db
+        :param Query: Query to extract information
+    :return: Result of query
+    '''
+
+    result = []
+
+    conn = sqlite3.connect(Path)
     cur = conn.cursor()
-    rows = cur.execute(query).fetchall()
-    return rows
+    rows = cur.execute(Query).fetchall()
 
-def selectQueryMulti(dbPath, query):
-    geolocation = []
-    geolocation.append(['Lat', 'Long', 'Name'])
-
-    conn = sqlite3.connect(dbPath)
-    cur = conn.cursor()
-    rows = cur.execute(query).fetchall()
     for row in rows:
-        geolocation.append(list(row))
-    return geolocation
+        result.append(list(row))
+
+    return result
 
 
-#-----Interaction with Google APi
 
-def GoogleInteraction(datos, mac):
-    YOUR_API_KEY = "AIzaSyBgqJdq2ZB2wxaYyxrJBnZeFC0ytINjaUE"
-    URL = "https://www.googleapis.com/geolocation/v1/geolocate?key="
+def GoogleGeolocationAPI(Data, Name):
+    '''
+    This fuction interacts with Google API, get precision and acurrency of data
+        :param Data: json for request to Google API
+        :param Name: Mac for represent in Google Maps
+    :return:  Array with latitude, longitude and name
+    '''
 
-    response = requests.post(URL+YOUR_API_KEY, json=datos)
+    response = requests.post(URL+YOUR_API_KEY, json=Data)
 
     if response.status_code == 200:
-        latitud = response.json()['location']['lat']
-        longitud = response.json()['location']['lng']
+        lat = response.json()['location']['lat']
+        long = response.json()['location']['lng']
 
-        return [latitud, longitud, mac]
-    else:
-        return [40.4377317, -3.2868383999999997, '72688506']
+        return [lat, long, Name]
 
-"""
-def AccesPointWifiTriangulation(bssid):
-    datos = {
-        "considerIp": "false",
-        "wifiAccessPoints": [
-            {
-                "macAddress": "%s" % bssid[1],
-                "signalStrength": -48,
-                "signalToNoiseRatio": 0
-            },
-            {
-                "macAddress": "%s" % bssid[2],
-                "signalStrength": -49,
-                "signalToNoiseRatio": 0
-            }
-        ]
-    }
 
-"""
 
-#-----Diferent types of get Geolocation
+
 def DireccionIP():
+    '''
+    First method for get position of any device. Get lat, long and acurrency of IP
+    :return:Array with latitude, longitude and name
+    '''
+
     datos = {
         "considerIp": "true"
     }
     return datos
 
 
-def AccesPointWifi(bssid):
-    geolocation = []
+def AccesPointWifi(Bssid):
+    '''
+    Second method for get position of any device. Get lat, long and acurrency  with
+    Wardriving thecnique.
+        :param Bssid: Unique name to identificate a Wifi
+    :return:Array with latitude, longitude and name
+    '''
 
+    geolocation = []
     geolocation.append(['Lat', 'Long', 'Name'])
 
-    for mac in bssid:
+    for mac in Bssid:
         if mac is not None:
             datos = {
                 "wifiAccessPoints": [
@@ -83,33 +87,64 @@ def AccesPointWifi(bssid):
                     }
                 ]
             }
-            geolocation.append(GoogleInteraction(datos, mac))
+            geolocation.append(GoogleGeolocationAPI(datos, mac))
 
     return geolocation
 
 
+def AccesPointWifiTriangulation(Bssid):
+    '''
+    Second method for get position of any device. Is better because use technical triangulation.
+        :param Bssid: Unique name to identificate a Wifi
+    :return:Array with latitude, longitude and name
+    '''
 
-def TowerCell(bssid):
     """
-    #Example: 'gsm:214:03:9150:2401'
+    geolocation = []
+    geolocation.append(['Lat', 'Long', 'Name'])
 
-    cellId = 73628675
-    locationAreaCode = 268
-    mobileCountryCode = 214,
-    mobileNetworkCode = 1
+    for mac in Bssid:
+        datos = {
+            "considerIp": "false",
+            "wifiAccessPoints": [
+                {
+                    "macAddress": "%s" % Bssid[1],
+                    "signalStrength": -48,
+                    "signalToNoiseRatio": 0
+                },
+                {
+                    "macAddress": "%s" % Bssid[2],
+                    "signalStrength": -49,
+                    "signalToNoiseRatio": 0
+                }
+            ]
+        }
+        geolocation.append(GoogleGeolocationAPI(datos, mac))
 
-    gsm[0] = gsm
-    gsm[1] = 214
-    gsm[2] = 03
-    gsm[3] = 9150
-    gsm[4] = 2401
+    return geolocation
     """
 
+
+def TowerCell(CellTower):
+    '''
+    Third method for get position of any device. Get lat, long and acurrency  with GSM. LTE or CDMA log's.
+        :param CellTower: Recive CellID, Location area, Mobile Country Code and mobile Network
+    :return:Array with latitude, longitude and name
+
+    The log's recived "gsm:214:03:9150:2401"
+        type = gsm
+        mobileCountryCode (MCC) = 214
+        mobileNetworkCode (MNC) = 03
+        locationAreaCode  (LAC) = 9150
+        cellId            (CID) = 2401
+
+    https://es.wikipedia.org/wiki/MCC/MNC
+    '''
 
     geolocation = []
     geolocation.append(['Lat', 'Long', 'Name'])
 
-    for gsm in bssid:
+    for gsm in CellTower:
         if gsm.startswith('gsm') or gsm.startswith('lte') or gsm.startswith('cdma'):
             gsm = gsm.split(":")
             datos = {
@@ -124,63 +159,60 @@ def TowerCell(bssid):
                 ]
             }
 
-            geolocation.append(GoogleInteraction(datos, gsm[4]))
+            geolocation.append(GoogleGeolocationAPI(datos, gsm[4]))
 
     return geolocation
 
 
-def outputHtml(name, arrayData):
+
+def outputHtml(Name, ArrayData):
+    '''
+    Print the GeoData in html geo chart like a map.
+        :param Name: Name of output file
+        :param ArrayData: Array with latitude, longitude and name
+    :return: the output is a html
+    '''
+
     file = open('templates/index.html', 'r')
     text = file.read()
-    geoWifi = text.replace('{{ array }}', arrayData)
-
+    geoWifi = text.replace('{{ array }}', ArrayData)
     file.close()
 
-    file = open("templates/%s.html"%name, "w")
+
+    file = open("templates/%s.html"%Name, "w")
     file.write(geoWifi)
     file.close()
 
 
+
+
 if __name__ == '__main__':
 
-    #----Queries-------
-    wigleWifi = "select lastlat, lastlon, bssid from network group by bssid"
-
-    herrevadWifi = "select bssid from local_reports group by bssid"
-    herrevadCellTowerQuery = "select rowkey from lru_table group by rowkey"
-
-
-    #------Argeparse------
     parser = argparse.ArgumentParser(prog='GeoForensic', description='Example with long option names', usage='python3 GeoForensic.py [options]')
-
     parser.add_argument('--db_path','-db', help="Path of sqlite")
-    parser.add_argument('--type','-t', help="herrevad or wiglewifi")
-
+    parser.add_argument('--type','-t', help="herrevad or wigle")
 
     args = parser.parse_args()
+    dbPath = args.db_path
 
-    if not args.db_path:
-        datos = DireccionIP()
+    if os.path.exists(dbPath):
+        if args.type == "herrevad":
+            LocationBssid = AccesPointWifi(SQLite(dbPath, HERREAVAD_WIFI_QUERY))
+            outputHtml("HERREAVAD_WIFI",str(LocationBssid))
 
-    else:
-        dbPath = args.db_path
-        if os.path.exists(dbPath):
-            if args.type == "herrevad":
-                LocationBssid = AccesPointWifi(selectQuery(dbPath, herrevadWifi))
-                outputHtml("herrevadWifi",str(LocationBssid))
+            CellTower = TowerCell(SQLite(dbPath, HERREAVAD_CELL_QUERY))
+            outputHtml("HERREAVAD_CELL",str(CellTower))
 
-                cellTower = TowerCell(selectQuery(dbPath, herrevadCellTowerQuery))
-                outputHtml("herrevadCellTower",str(cellTower))
-
-            elif args.type == "wiglewifi":
-                LocationBssid = selectQueryMulti(dbPath, wigleWifi)
-                outputHtml("wigleWifi", str(LocationBssid))
-
-            else:
-                print("No has introducido un tipo correcto")
-
-
+        elif args.type == "wigle":
+            LocationBssid = SQLite(dbPath, WIGLE_WIFI_QUERY)
+            LocationBssid.insert(0, ['Lat', 'Long', 'Name'])
+            outputHtml("WIGLE_WIFI", str(LocationBssid))
 
         else:
-            print("No existe la ruta")
+            print("Please, use a correct type")
+
+
+
+    else:
+        print("Doesn't exit path")
 
